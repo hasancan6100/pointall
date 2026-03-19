905c745ac4ae5988b13362faab24383769a731b2
hasancan6100 <hasanmuhsin910@gmail.com>
2026-03-20 01:48:10 +0300

    Fix package name to match PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="phone-data-collector",
+    name="pointall",
     version="1.0.1",
     description="Pydroid için telefon verisi toplama kütüphanesi",
     author="Your Name",
